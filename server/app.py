2712020d7b0dec52abfb89a3fe9201b7b286f7bf
Pratham Bhonge <prathambhonge@Prathams-MacBook-Pro.local>
2023-07-11 14:26:35 +0530

    LFGGGGGGGGGG

--- server/app.py
+++ server/app.py
@@ -1,3 +1,43 @@
+# from flask import Flask, request, jsonify
+# from upload_handler import process_csv_files, upload_json_file, retrieve_output_text
+# from flask_cors import CORS
+# import json
+
+# app = Flask(__name__)
+# cors = CORS(app)
+
+# # Handle file upload and processing
+
+
+# @app.route('/upload', methods=['POST'])
+# def handle_upload():
+#     files = request.files.getlist('files')
+
+#     if all(file.filename.endswith('.csv') for file in files):
+#         # Process the CSV files and generate output
+#         output_data = process_csv_files(files)
+
+#         # Upload the output as a JSON file to Firebase Storage
+#         filename = upload_json_file(output_data)
+
+#         return jsonify({'filename': filename})
+
+#     return jsonify({'error': 'Invalid file format'})
+
+
+# @app.route('/api/output/latest', methods=['GET'])
+# def get_latest_output_text():
+#     data = retrieve_output_text()
+#     if data is not None:
+#         data_dict = json.loads(data)
+#         output = data_dict['output']
+#         if output is not None:
+#             return jsonify({'outputText': output})
+#     return jsonify({'error': 'Error retrieving output text'})
+
+
+# if __name__ == '__main__':
+#     app.run()
 from flask import Flask, request, jsonify
 from upload_handler import process_csv_files, upload_json_file, retrieve_output_text
 from flask_cors import CORS
--- server/upload_handler.py
+++ server/upload_handler.py
@@ -5,8 +5,10 @@ import openai
 import datetime
 import json
 import re
+import psycopg2
+from io import StringIO
 
-openai.api_key = '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'
+openai.api_key = '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'
 
 # Initialize Firebase Admin SDK
 # Update with your own service account key path
@@ -22,15 +24,23 @@ firebase_admin.initialize_app(cred, {
 
 def process_csv_files(files):
     combined_columns = ""
+    tables = []
+
     for i, file in enumerate(files, start=1):
         df = pd.read_csv(file)
         columns = df.columns.tolist()
-        table_name = f"Table {i}"
+        table_name = f"Table_{i}"
         combined_columns += f"{table_name} has following columns: {', '.join(columns)}\n"
+
+        tables.append({
+            'name': table_name,
+            'columns': columns,
+            'data': df
+        })
 
     try:
         prompt = "I have multiple tables. The column names of all these tables are as follows: " + \
-                 combined_columns + ". Generate appropriate relations between these tables. Name the primary key and foreign key. Draw lines between relations. Mention if it's a one-to-one relation or the type of relationship."
+            combined_columns + ". Generate appropriate relations between these tables. Name the primary key and foreign key. Draw lines between relations. Mention if it's a one-to-one relation or the type of relationship."
 
         response = openai.Completion.create(
             engine='text-davinci-003',
@@ -52,7 +62,51 @@ def process_csv_files(files):
         'output': output
     }
 
+    # Store tables in PostgreSQL database
+    store_tables_in_database(tables)
+
     return data
+
+# Store the tables in the PostgreSQL database
+
+
+def store_tables_in_database(tables):
+    # Update the PostgreSQL connection details as per your database configuration
+    conn = psycopg2.connect(
+        host='trumpet.db.elephantsql.com',
+        database='dykrojjj',
+        user='dykrojjj',
+        password='x0tLUyYZUdP3jmM4borqLnMMH2S-Lc7q'
+    )
+
+    try:
+        cursor = conn.cursor()
+
+        for table in tables:
+            table_name = table['name']
+            columns = table['columns']
+            data = table['data']
+
+            # Create the table with the same column names as CSV
+            create_table_query = f"CREATE TABLE {table_name} ({', '.join([f'{col} VARCHAR(255)' for col in columns])});"
+            cursor.execute(create_table_query)
+
+            # Prepare the data for bulk insertion
+            data_buffer = StringIO()
+            data.to_csv(data_buffer, index=False, header=False)
+            data_buffer.seek(0)
+
+            # Copy the data into the table
+            copy_query = f"COPY {table_name} FROM STDIN WITH (FORMAT CSV, HEADER FALSE);"
+            cursor.copy_expert(copy_query, data_buffer)
+
+        conn.commit()
+    except Exception as e:
+        print('Error storing tables in the database:', str(e))
+        conn.rollback()
+    finally:
+        cursor.close()
+        conn.close()
 
 # Upload the output data as a JSON file to Firebase Storage
 
